74d7234b3f75b9ec8cd74f84c9a324616c40c42d
John Doe <pehlivanfurkan93@gmail.com>
2024-07-30 16:15:28 +0300

    S14d2-proje son

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,24 @@
 package org.example;
 
+import org.example.model.*;
+import org.example.model.enums.LampType;
+import org.example.model.enums.PaintColor;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+         Wall wall1 = new Wall("Kuzey");
+        Wall wall2 = new Wall("Guney");
+        Wall wall3 = new Wall("Dogu");
+        Wall wall4 = new Wall("Bati");
+        Ceiling ceiling= new Ceiling(3, PaintColor.WHITE);
+        Bed bed = new Bed("King",4,60,2,1);
+        Lamp lamp = new Lamp(LampType.LAVA,true,2);
+        Wardrobe wardrobe = new Wardrobe(2,2,45.0);
+        Carpet carpet=new Carpet(3,3,PaintColor.GREEN);
+        Bedroom bedroom= new Bedroom("King Room",wall1,wall2,wall3,wall4,ceiling,bed,lamp,wardrobe,carpet);
+
+        System.out.println(bedroom);
+
     }
 }
